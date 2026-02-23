daca99401fac70863334aaeff1eab9d2e2d3227e
Noah Sparks <mathsquirrel@gmail.com>
2026-02-23 13:34:53 -0700

    Implemented basic logic for user data access functions

--- server/src/main/java/dataaccess/MemoryUserAccess.java
+++ server/src/main/java/dataaccess/MemoryUserAccess.java
@@ -2,16 +2,27 @@ package dataaccess;
 
 import model.UserData;
 
-public class MemoryUserAccess implements UserAccess{
-    public void createUser(UserData u) throws DataAccessException {
+import java.util.ArrayList;
+import java.util.Collection;
 
+public class MemoryUserAccess implements UserAccess{
+    Collection<UserData> userList = new ArrayList<>(); // List of UserData objects or users
+    public void createUser(UserData u) throws DataAccessException {
+        // Add user "u" to userList
+        userList.add(u);
     }
 
     public UserData getUser(UserData u) throws DataAccessException {
-        return null;
+        // If user exists in database, return it
+        if(userList.contains(u)){
+            return u;
+        }else{
+            return null;
+        }
     }
 
     public void clear() throws DataAccessException {
-
+        // Clears users from database
+        userList = new ArrayList<>();
     }
 }
